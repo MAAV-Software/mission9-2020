19c80e0ec1d2cc56f325c73b9e59b62b095077ce
Joe Macek <macekj@umich.edu>
2019-12-04 20:10:41 -0500

    Setup px4_sitl in docker + bash interactive terminal

--- scripts/setup-sim.sh
+++ scripts/setup-sim.sh
@@ -55,3 +55,19 @@ if [ ! -f exe/QGroundControl.AppImage ]; then
 	wget https://s3-us-west-2.amazonaws.com/qgroundcontrol/builds/master/QGroundControl.AppImage -P /bin
 	chmod a+x /bin/QGroundControl.AppImage
 fi
+
+
+# Install a bunch of dependencies for PX4_SITL
+sudo apt-get install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libgstreamer1.0-0 libgstreamer1.0-dev
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
+sudo apt-get install libprotobuf-dev libprotoc-dev protobuf-compiler libeigen3-dev libxml2-utils python-rospkg python-jinja2
+
+# Clone and build PX4_SITL
+cd /
+mkdir -p /px4_sitl && cd px4_sitl
+git clone --recursive https://github.com/PX4/sitl_gazebo.git
+cd sitl_gazebo
+mkdir build && cd build
+cmake ..
+make -j
+sudo make install

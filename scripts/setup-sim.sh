f1f15683cbe3e420479d2ebf4bb7b67b6f49f31d
Joe Macek <macekj@umich.edu>
2019-12-08 15:00:21 -0500

    fix bugs in sim setup script

--- scripts/setup-sim.sh
+++ scripts/setup-sim.sh
@@ -58,9 +58,9 @@ fi
 
 
 # Install a bunch of dependencies for PX4_SITL
-sudo apt-get install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libgstreamer1.0-0 libgstreamer1.0-dev
-sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
-sudo apt-get install libprotobuf-dev libprotoc-dev protobuf-compiler libeigen3-dev libxml2-utils python-rospkg python-jinja2
+sudo apt-get install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libgstreamer1.0-0 libgstreamer1.0-dev -y
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras -y
+sudo apt-get install libprotobuf-dev libprotoc-dev protobuf-compiler libeigen3-dev libxml2-utils python-rospkg python-jinja2 -y
 
 # Clone and build PX4_SITL
 cd /
@@ -68,6 +68,7 @@ mkdir -p /px4_sitl && cd px4_sitl
 git clone --recursive https://github.com/PX4/sitl_gazebo.git
 cd sitl_gazebo
 mkdir build && cd build
+CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:/usr/bin/gazebo
 cmake ..
 make -j
 sudo make install

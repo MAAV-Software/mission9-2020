be12ca6cc8b77041ba87e77a1b5c882a0eaf0eee
macekj <46473161+macekj@users.noreply.github.com>
2020-01-08 20:19:20 -0500

    PX4 SITL setup (#3)
    * PX4 SITL setup
    
    Co-authored-by: Romario Pashollari <rpash@umich.edu>

--- scripts/setup-ros-source.sh
+++ scripts/setup-ros-source.sh
@@ -1,2 +1,15 @@
 source /opt/ros/melodic/setup.bash
-source /mission9/workspace/devel/setup.bash
+source /mission9/workspace/devel/setup.bash || echo "Try running catkin_make all in /workspace"
+
+# Source gazebo setup.sh
+source /usr/share/gazebo/setup.sh
+
+# Set the plugin path so Gazebo finds our model and sim
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/px4_sitl/sitl_gazebo/build
+# Set the model path so Gazebo finds the airframes
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:/px4_sitl/sitl_gazebo/models
+# Disable online model lookup since this is quite experimental and unstable
+export GAZEBO_MODEL_DATABASE_URI=""
+
+# Set path to sitl_gazebo repository
+export SITL_GAZEBO_PATH=/px4_sitl/sitl_gazebo
--- scripts/setup-sim.sh
+++ scripts/setup-sim.sh
@@ -55,3 +55,20 @@ if [ ! -f exe/QGroundControl.AppImage ]; then
 	wget https://s3-us-west-2.amazonaws.com/qgroundcontrol/builds/master/QGroundControl.AppImage -P /bin
 	chmod a+x /bin/QGroundControl.AppImage
 fi
+
+
+# Install a bunch of dependencies for PX4_SITL
+sudo apt-get install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libgstreamer1.0-0 libgstreamer1.0-dev -y
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras -y
+sudo apt-get install libprotobuf-dev libprotoc-dev protobuf-compiler libeigen3-dev libxml2-utils python-rospkg python-jinja2 -y
+
+# Clone and build PX4_SITL
+cd /
+mkdir -p /px4_sitl && cd px4_sitl
+git clone --recursive https://github.com/PX4/sitl_gazebo.git
+cd sitl_gazebo
+mkdir build && cd build
+CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:/usr/bin/gazebo
+cmake ..
+make -j
+sudo make install

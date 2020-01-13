a3b05a2cf069b7cefec03544e8fc6a4a6f01a133
Ryan Wunderly <rywunder@umich.edu.com>
2020-01-13 16:55:17 -0500

    remove redundant ros installs

--- scripts/setup-px4.sh
+++ scripts/setup-px4.sh
@@ -22,10 +22,8 @@ make install
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt update
-apt-get install -y ros-melodic-desktop-full
 source /opt/ros/melodic/setup.bash
 
-apt-get install -y ros-melodic-gazebo9-*
 rosdep init
 rosdep update
 

e70a5f4cd45b422d0a483f20afc258ee4213dcc4
Jacob Minock <jaminock@umich.edu>
2020-10-29 01:46:21 -0400

    Docker image build now deletes ~6GB of build stuff
    The temporary files for building OpenCV and Realsense from source are
    now deleted at the end of the script where that build occurs. Additionally,
    the apt-get commands have been moved to one apt-get command because it doesn't
    make sense to run the overhead of each apt-get command for every installed
    dependency. The apt lists are also deleted now, freeing about 40MB.

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -7,50 +7,53 @@ apt-get dist-upgrade
 # Place apt in non interactive mode
 DEBIAN_FRONTEND=noninteractive
 
-# Install required packages (please use an apt-get command for each package)
-apt-get install -y pkg-config # Package config
-apt-get install -y git # Version control
-apt-get install -y cmake # Build system
-apt-get install -y unzip # Extract zip files
-apt-get install -y software-properties-common # Managing apt resources
-apt-get install -y python-software-properties # Managing pip resources
-apt-get install -y cmake-curses-gui # CMake config GUI
-apt-get install -y clang-format # Code formatter
-apt-get install -y curl # Downloading stuff from websites
-apt-get install -y libcurl4-openssl-dev # OpenSSL support for curl
-apt-get install -y wget # Downloading stuff from websites, Stallman version
-apt-get install -y doxygen # Documentation
-apt-get install -y libyaml-cpp-dev # YAML for config files
-apt-get install -y ffmpeg # Image processing library
-apt-get install -y libglew-dev # OpenGL extension interface
-apt-get install -y libglfw3-dev # OpenGL Utilities
-apt-get install -y libboost-test-dev # Boost test
-apt-get install -y libboost-program-options-dev # Boost program options
-apt-get install -y libev-dev # Event loop (IO handling)
-apt-get install -y libgtkmm-3.0-dev # GTK GUI, CPP version
-apt-get install -y libudev-dev # Device manager for Linux kernel
-apt-get install -y libglm-dev # OpenGL Math library
-apt-get install -y libusb-1.0-0-dev # USB interface for applications
-apt-get install -y libusb-1.0-doc # Documentation for libusb
-apt-get install -y libusb-1.0-0-dbg # Debug stuff for libusb
-apt-get install -y python3 # Python 3
-apt-get install -y python3-pip # Python 3 package manager
-apt-get install -y python-pip # Python 2 package manager
-apt-get install -y libvtk6-dev # Visualization toolkit
-apt-get install -y libopenni2-dev # Framework for sensor interaction
-apt-get install -y libssl-dev # SSL and TLS support
-apt-get install -y libeigen3-dev # Linear algebra library
-apt-get install -y libflann1.9 # Fast nearest neighbor
-apt-get install -y libflann-dev # Fast nearest neighbor dev
-apt-get install -y libboost-all-dev # Boost library
-apt-get install -y libproj-dev # Projection library
-apt-get install -y libqhull-dev # Convex hull library
-apt-get install -y libpcl-* # Point Could Library
-apt-get install -y gcc5 # GCC 5 required to build some libraries
-apt-get install -y sl # system libraries
-apt-get install -y tmux # terminal manager
-apt-get install -y vim # l33t ide
-apt-get install -y python-catkin-tools # catkin CLI tools
+# Install required packages
+# Previously used an apt-get command for each package
+apt-get install -y \
+    clang-format \                  # Code formatter
+    cmake \                         # Build system
+    cmake-curses-gui \              # CMake config GUI
+    curl \                          # Downloading stuff from websites
+    doxygen \                       # Documentation
+    ffmpeg \                        # Image processing library
+    gcc5 \                          # GCC 5 required to build some libraries
+    git \                           # Version control
+    libboost-all-dev \              # Boost library
+    libboost-program-options-dev \  # Boost program options
+    libboost-test-dev \             # Boost test
+    libcurl4-openssl-dev \          # OpenSSL support for curl
+    libeigen3-dev \                 # Linear algebra library
+    libev-dev \                     # Event loop (IO handling)
+    libflann1.9 \                   # Fast nearest neighbor
+    libflann-dev \                  # Fast nearest neighbor dev
+    libglew-dev \                   # OpenGL extension interface
+    libglfw3-dev \                  # OpenGL Utilities
+    libglm-dev \                    # OpenGL Math library
+    libgtkmm-3.0-dev \              # GTK GUI, CPP version
+    libopenni2-dev \                # Framework for sensor interaction
+    libpcl-* \                      # Point Could Library
+    libproj-dev \                   # Projection library
+    libqhull-dev \                  # Convex hull library
+    libssl-dev \                    # SSL and TLS support
+    libudev-dev \                   # Device manager for Linux kernel
+    libusb-1.0-0-dbg \              # Debug stuff for libusb
+    libusb-1.0-0-dev \              # USB interface for applications
+    libusb-1.0-doc \                # Documentation for libusb
+    libvtk6-dev \                   # Visualization toolkit
+    libyaml-cpp-dev \               # YAML for config files
+    pkg-config \                    # Package config
+    python3 \                       # Python 3
+    python3-pip \                   # Python 3 package manager
+    python-catkin-tools \           # catkin CLI tools
+    python-pip \                    # Python 2 package manager
+    python-software-properties \    # Managing pip resources
+    sl \                            # system libraries
+    software-properties-common \    # Managing apt resources
+    tmux \                          # terminal manager
+    unzip \                         # Extract zip files
+    vim \                           # l33t ide
+    wget \                          # Downloading stuff from websites
+    && rm -rf /var/lib/apt/lists/*  # -- Free space up (about 40MB) --
 
 num_procs_avail=$(($(grep -c ^processor /proc/cpuinfo)-1))
 num_procs_avail=$((num_procs_avail > 1 ? num_procs_avail : 1))
@@ -70,25 +73,24 @@ modprobe -r videodev
 cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 udevadm control --reload-rules && udevadm trigger
 ./scripts/patch-realsense-ubuntu-lts.sh
-
 # Build and install
 mkdir build && cd build
 cmake ..
 make -j$num_procs_avail
 make install
+# TODO Delete Realsense source?
+cd /
+rm -rf /tmp/librealsense
 
 # Install OpenCV 3.3.1
 # ------------------------
 cd /tmp
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-# Checkout version 3.3.1
+git clone --depth 1 --branch 3.3.1 https://github.com/opencv/opencv.git
+git clone --depth 1 --branch 3.3.1 https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
-git checkout tags/3.3.1
 mv modules/dnn_modern/CMakeLists.txt \
    modules/dnn_modern/CMakeLists.txt.bak
 cd ../opencv
-git checkout tags/3.3.1
 # Build and install - this will take a lot of space and time
 mkdir build
 cd build
@@ -106,3 +108,6 @@ cmake -D BUILD_PNG=ON \
       ..
 make -j$num_procs_avail
 make install
+# TODO Delete OpenCV 3.3.1 source?
+cd /
+rm -rf /tmp/opencv /tmp/opencv_contrib
